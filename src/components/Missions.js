386d0f25cf7eab5e541c8a53c19f74a00c6fcd96
cjheyde <carlaheyde@gmail.com>
2022-04-06 16:56:30 -0300

    requirement 7

---
+++ src/components/Missions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/Missions.css';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <h1>Mission</h1>
+      </div>
+    );
+  }
+}
+
+export default Missions;
