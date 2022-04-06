0fbabf70d4a6237c015fccca481311197d9e45ce
cjheyde <carlaheyde@gmail.com>
2022-04-06 14:38:21 -0300

    requisito 1 com css

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/Header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="Header">
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
